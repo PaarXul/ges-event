58e6e8ee87cab3879a829e6694b8aff3a6d0ec9d
PaarXul <gian.parra96@gmail.com>
2024-06-18 21:20:04 -0400

    Primer commit del proyecto inicial Spring

---
+++ src/main/java/cl/event/app/gestioneventos/GestioneventosApplication.java
@@ -0,0 +1,13 @@
+package cl.event.app.gestioneventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/event/app/gestioneventos/GestioneventosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.event.app.gestioneventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
